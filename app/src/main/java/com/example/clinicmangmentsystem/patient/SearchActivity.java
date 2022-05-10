75e6cb584d21d631cd007c25d2e4e3ac872dccbb
midoel3ila <Abdelhamedelila@gmail.com>
2022-05-10 02:09:27 +0200

    Initial open source commit

---
+++ app/src/main/java/com/example/clinicmangmentsystem/patient/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.clinicmangmentsystem.patient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.clinicmangmentsystem.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
